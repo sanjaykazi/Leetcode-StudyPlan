df2eecd1f3292c628565ec6d0f15685414e8dda6
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-10-29 05:54:14 +0530

    Day-2 sorted array square

---
+++ Algorithm/sortedArraySquare.cpp
@@ -0,0 +1,8 @@
+vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> res;
+        for(int i = 0; i < nums.size(); i++){
+            res.push_back(nums[i]*nums[i]);
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
