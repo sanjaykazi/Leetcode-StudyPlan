b4db93001f08c488933c680d6ea71c65926071a6
Sanjay Kazi <sanjaykazzi1149@gmail.com>
2021-10-29 17:59:55 +0530

    Day-1 maximum subarray sum

---
+++ Data structures/maxSubArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxSubArray(vector<int> &arr)
+{
+  int n = arr.size();
+  int max_so_far = arr[0];
+  int max_ending_here = arr[0];
+  int i;
+  for (i = 1; i < n; i++)
+  {
+    max_ending_here = max(arr[i], max_ending_here + arr[i]);
+    max_so_far = max(max_so_far, max_ending_here);
+  }
+  return max_so_far;
+}
