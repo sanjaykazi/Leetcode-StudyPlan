952f8b50aaf686ce47454c7648db6e335226b537
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-11-01 05:03:57 +0530

    Day-4 reverse word in an string

---
+++ wordreverseinstring.cpp
@@ -0,0 +1,13 @@
+string reverseWords(string s) {
+        stringstream ss(s);
+
+        string result = "";
+        string token = "";
+
+        while(ss >> token) {
+            reverse(begin(token), end(token));
+            result += token + " ";
+        }
+
+        return result.substr(0, result.length()-1);
+    }
