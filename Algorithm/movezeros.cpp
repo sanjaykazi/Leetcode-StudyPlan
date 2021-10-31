210d8fd7b96e6f6d40b9f2d84fcec5876419ebb1
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-11-01 04:28:26 +0530

    Day-3 move zeros

---
+++ Algorithm/movezeros.cpp
@@ -0,0 +1,11 @@
+void moveZeroes(vector<int>& nums) {
+        int k = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++)
+            if (nums[i] != 0)
+                nums[k++] = nums[i];
+        
+        for (int i = k; i < n; i++)
+            nums[i] = 0;
+    }
