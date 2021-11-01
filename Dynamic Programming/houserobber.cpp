fafa00745e077d2eb35e3e12dd42b91f8dc62582
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-11-01 06:00:45 +0530

    Day-3 house robber I

---
+++ Dynamic Programming/houserobber.cpp
@@ -0,0 +1,12 @@
+int dp[101];
+    int maxrob(vector<int> &nums, int i){
+        if(i<0) return 0;
+        if(dp[i] >= 0) return dp[i];
+        return dp[i] = max(maxrob(nums,i-2)+nums[i],maxrob(nums,i-1));
+    }
+    int rob(vector<int>& nums) {
+        for(int i = 0; i<101;i++){
+            dp[i] = -1;
+        }
+        return maxrob(nums,nums.size()-1);
+    }
