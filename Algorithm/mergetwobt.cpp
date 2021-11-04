09300a116be4e07dc0247c00920cd706c0f1f258
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-11-04 19:28:34 +0530

    Day-8 merge two binary trees

---
+++ Algorithm/mergetwobt.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (!t1 || !t2) return t1 ? t1 : t2;
+
+        TreeNode* node = new TreeNode(t1->val + t2->val);
+        node->left = mergeTrees(t1->left, t2->left);
+        node->right = mergeTrees(t1->right, t2->right);
+        return node;
+    }
+};
