362315bda8cd2dc12365d92628b3890ee878671e
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-11-05 01:36:40 +0530

    Day-7 merge two sorted linked list

---
+++ Data structures/mergetwosortedll.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(NULL == l1) return l2;
+    if(NULL == l2) return l1;
+    
+    ListNode* head=NULL;    // head of the list to return
+    
+    // find first element (can use dummy node to put this part inside of the loop)
+    if(l1->val < l2->val)       { head = l1; l1 = l1->next; }
+    else                        { head = l2; l2 = l2->next; }
+    
+    ListNode* p = head;     // pointer to form new list
+    
+    // I use && to remove extra IF from the loop
+    while(l1 && l2){
+        if(l1->val < l2->val)   { p->next = l1; l1 = l1->next; }
+        else                    { p->next = l2; l2 = l2->next; }
+        p=p->next;
+    }
+    
+    // add the rest of the tail, done!
+    if(l1)  p->next=l1;
+    else    p->next=l2;
+    
+    return head;
+    }
+};
+// class Solution {
+// public:
+//    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+//     if (l1 == NULL) return l2;
+//     if (l2 == NULL) return l1;
+//     if (l1->val <= l2->val) {
+//         l1->next = mergeTwoLists(l1->next, l2);
+//         return l1;
+//     } else {
+//         l2->next = mergeTwoLists(l1, l2->next);
+//         return l2;
+//     }
+// }
+// };
