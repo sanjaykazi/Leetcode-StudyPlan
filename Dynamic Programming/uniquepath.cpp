8f2689330ec0746a418ef15576ab30cbbf681c0a
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-11-13 03:37:33 +0530

    Day-15 unique path

---
+++ Dynamic Programming/uniquepath.cpp
@@ -0,0 +1,25 @@
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<int> pre(n, 1), cur(n, 1);
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 1; j < n; j++) {
+//                 cur[j] = pre[j] + cur[j - 1];
+//             }
+//             swap(pre, cur);
+//         }
+//         return pre[n - 1];
+//     }
+// };
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> cur(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }
+};
