ebd74fe3c46f29dabed66d4f45b10e5f08e4cb10
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-11-13 22:57:18 +0530

    Day-2 hash map implementation

---
+++ Data structure - 2/hashmapimplementation.cpp
@@ -0,0 +1,67 @@
+struct Node {
+public:
+    int key, val;
+    Node* next;
+    Node(int k, int v, Node* n) {
+        key = k;
+        val = v;
+        next = n;
+    }
+};
+class MyHashMap {
+public:
+    const static int size = 19997;
+    const static int mult = 12582917;
+    Node* data[size];
+    int hash(int key) {
+        return (int)((long)key * mult % size);
+    }
+    void put(int key, int val) {
+        remove(key);
+        int h = hash(key);
+        Node* node = new Node(key, val, data[h]);
+        data[h] = node;
+    }    
+    int get(int key) {
+        int h = hash(key);
+        Node* node = data[h];
+        for (; node != NULL; node = node->next)
+            if (node->key == key) return node->val;
+        return -1;
+    }    
+    void remove(int key) {
+        int h = hash(key);
+        Node* node = data[h];
+        if (node == NULL) return;
+        if (node->key == key) data[h] = node->next;
+        else for (; node->next != NULL; node = node->next)
+            if (node->next->key == key) {
+                node->next = node->next->next;
+                return;
+            }
+    }
+};
+// class MyHashMap {
+// public:
+//     int data[1000001];
+//     MyHashMap() {
+//         fill(data, data + 1000000, -1);
+//     }
+//     void put(int key, int val) {
+//         data[key] = val;
+//     }
+//     int get(int key) {
+//         return data[key];
+//     }
+//     void remove(int key) {
+//         data[key] = -1;
+//     }
+// };
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
