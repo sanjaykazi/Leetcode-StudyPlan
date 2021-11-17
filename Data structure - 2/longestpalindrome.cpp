d99a1685214548e1c5d0138b413521e5adca5466
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-11-18 02:28:25 +0530

    Day-6 longest palindrome

---
+++ Data structure - 2/longestpalindrome.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+    vector<int> count(256);
+    for (char c : s)
+        ++count[c];
+    int odds = 0;
+    for (int c : count)
+        odds += c & 1;
+    return s.size() - odds + (odds > 0);
+}
+
+};
