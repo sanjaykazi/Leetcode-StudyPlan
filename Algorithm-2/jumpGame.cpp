15891ebaeb7a0f9d804a18cfe3f5319892883ebd
Sanjay Kazi <51957689+sanjaykazi@users.noreply.github.com>
2021-11-23 02:16:47 +0530

    Day-12 jump game

---
+++ Algorithm-2/jumpGame.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int feasible = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(feasible < i) return false;
+            
+            feasible = max(feasible, nums[i]+i);
+        }
+        return true;
+    }
+};
